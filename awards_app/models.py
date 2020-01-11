5e81980462e0bfbf1d1eeaa51438dfbb58bf0f1f
jakesIII <jayruma@yahoo.com>
2020-01-11 12:38:34 +0300

    set up django authentication

--- awards_app/models.py
+++ awards_app/models.py
@@ -4,7 +4,7 @@ from pyuploadcare.dj.models import ImageField
 # Create your models here.
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    avi=ImageField(manualcrop='')
+    avi=ImageField(manual_crop='')
     bio=models.CharField(max_length=240)
     phone=models.IntegerField(blank=True)
     email=models.EmailField()
@@ -13,5 +13,5 @@ class Project(models.Model):
     user=models.ForeignKey(User)
     profile=models.ForeignKey(Profile)
     title=models.CharField(max_length=150)
-    landing=ImageField(manualcrop='')
+    landing=ImageField(manual_crop='')
     description=models.TextField()
