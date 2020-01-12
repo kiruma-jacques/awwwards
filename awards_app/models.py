513ef82fa56bee6dc565a0bee200f8e2da248e43
jakesIII <jayruma@yahoo.com>
2020-01-12 21:43:19 +0300

    adjust models for relevance

--- awards_app/models.py
+++ awards_app/models.py
@@ -3,15 +3,14 @@ from django.contrib.auth.models import User
 from pyuploadcare.dj.models import ImageField
 # Create your models here.
 class Profile(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
     avi=ImageField(manual_crop='')
     bio=models.CharField(max_length=240)
     phone=models.IntegerField(blank=True)
     email=models.EmailField()
 
 class Project(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    profile=models.ForeignKey(Profile,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='project')
     title=models.CharField(max_length=150)
     landing=ImageField(manual_crop='')
     description=models.TextField()
