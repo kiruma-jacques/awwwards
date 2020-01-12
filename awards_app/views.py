f0d730d6e67d7c7af698536e2e013943edcfdb57
jakesIII <jayruma@yahoo.com>
2020-01-12 21:32:48 +0300

    display the upload form on landing page

--- awards_app/views.py
+++ awards_app/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from .models import Project
-from .forms import ProjectUpload
+from .forms import ProjectUploadForm
 from django.http import HttpResponseRedirect
 
 # Create your views here.
-def index(request):
-    projects=Projects.objects.all()[::-1]
-    proj_upload=ProjectUpload(rrequest.POST)
+def index(request,**kwargs):
+    projects=Project.objects.all()[::-1]
+    proj_upload=ProjectUploadForm(request.POST, request.FILES)
     if proj_upload.is_valid():
         projo=proj_upload.save(commit=False)
         projo.user=request.user
-        upload.save()
+        projo.save()
         return HttpResponseRedirect(request.path_info)
-
-    return render(request, 'index.html')
+    else:
+        proj_upload=ProjectUploadForm()
+    context={
+        'projects':projects,
+        'proj_upload':proj_upload,
+    }
+    return render(request, 'index.html', locals())
