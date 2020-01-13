4d59c8be08f310f58c06e2ad1ce62841f1322dbe
jakesIII <jayruma@yahoo.com>
2020-01-13 14:52:10 +0300

    create review form

--- awards_app/forms.py
+++ awards_app/forms.py
@@ -1,5 +1,13 @@
 from django import forms
-from .models import Profile, Project
+from .models import Profile,Project,Review
+
+SCORES = [
+    (1,1),(2,2),
+    (3,3),(4,4),
+    (5,5),(6,6),
+    (7,7),(8,8),
+    (9,9),(10,10),
+]
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
@@ -10,3 +18,14 @@ class ProjectUploadForm(forms.ModelForm):
     class Meta:
         model=Project
         exclude=['user']
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model=Review
+        exclude=['user','project']
+        widget={
+            'comment':forms.Textarea(attrs={"class":"form-control mb-4"}),
+            'design':forms.Select(choices=SCORES,attrs={"class":"form-control mb-4"}),
+            'useability':forms.Select(choices=SCORES,attrs={"class":"form-control mb-4"}),
+            'content':forms.Select(choices=SCORES,attrs={"class":"form-control mb-4"}),
+        }
