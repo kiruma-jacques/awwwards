d592a7850cfe2ea7d35a693dbf980f86f13cb09f
jakesIII <jayruma@yahoo.com>
2020-01-14 22:06:44 +0300

    comment out a url with a bug

--- awards_app/urls.py
+++ awards_app/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 urlpatterns=[
     url(r'^$', views.index, name='homepage'),
     url(r'^profile/$', views.myProfile, name='profile'),
-    url(r'^<int:project_id>/review/$', views.review, name='review'),
+    # url(r'^<int:project_id>/review/$', views.review, name='review'),
 ]
 
 
